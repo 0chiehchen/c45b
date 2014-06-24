49b82a9415dd44dd0bc34022b2eb6e7b9db49070
John Lin <johnlin109@gmail.com>
2014-06-24 16:36:41 +0800

    Add Vagrant to the folder...   Not working yet..

---
+++ bootstrap.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# tools
+sudo apt-get install -y vim
+sudo apt-get install -y screen
+sudo apt-get install -y aptitude
+
+sudo apt-get install -y curl
+sudo apt-get install -y git-core
+
+sudo apt-get -y --force-yes install srecord
+sudo apt-get -y --force-yes install avrdude avrdude-doc
+sudo apt-get -y --force-yes install minicom picocom setserial
+
+# SERIAL TERMINAL
+sudo apt-get -y --force-yes install gtkterm moserial
+
+# Devel
+sudo apt-get -y --force-yes install build-essential
+sudo apt-get -y --force-yes install qt4-qmake
+
+# Install c45b and the required libqtcore4
+sudo apt-get -y --force-yes install libqtcore4
+#sudo dpkg -i /vagrant/tools/c45b/c45b_20131114-1_i386.deb
+
+# Add users to the dialout group
+sudo usermod -a -G dialout vagrant
+groups vagrant
+id vagrant
+
+echo "
+bootstrap.sh done...
+"
+
